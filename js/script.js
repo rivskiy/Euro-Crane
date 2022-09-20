3bd9fec9d7fad6f401be3c6847c8046eebc22653
Aleksey Pechurin <pe4urin@yandex.ru>
2022-09-20 14:07:05 +0300

    Дбавлено мобильное меню

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+//Mobile menu
+const menu = document.querySelector('.header__nav');
+const menuBtn = document.querySelector('.header__menu-btn');
+const menuOverlay = document.querySelector('.header__menu-overlay');
+const menuLink = document.querySelectorAll('.header__link');
 
+menuBtn.addEventListener('click', showMenu);
+
+menuOverlay.addEventListener('click', showMenu);
+
+function showMenu() {
+  menuBtn.classList.toggle('--active');
+  menu.classList.toggle('--active');
+  menuOverlay.classList.toggle('--active');
+  document.body.classList.toggle('--lock');
+}
